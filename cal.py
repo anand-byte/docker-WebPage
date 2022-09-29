78ff87622f0f8f346beb0a31e92f9a2d9c051e8f
Abhishek Anand <63868642+anand-byte@users.noreply.github.com>
2022-09-29 16:34:09 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import cgi
+import subprocess
+
+print("content-type:text/html")
+print()
+
+#x = "abhihsek"
+
+mydata = cgi.FieldStorage()
+op = mydata.getvalue("x")
+
+output = subprocess.getoutput("sudo " + op)
+
+print(output)
+
+#sub = subprocess.getoutput("cal")
+
+#print(sub)
